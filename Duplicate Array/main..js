2960aae8784096caf05006c8f7f86c41e5cc7aec
MageshVachiravel <mageshvachiravel@gmail.com>
2024-01-26 13:05:55 +0530

    docs: completed Duplicate Array

---
+++ Duplicate Array/main..js
@@ -0,0 +1,11 @@
+let array = [1,2,3,4,5,1,6,2,7,1,3];
+
+let uniqueArray = [];
+
+arr.forEach((number) => {
+    if(!unique.includes(number)){
+        unique.push(number)
+    }
+})
+
+console.log(unique)
